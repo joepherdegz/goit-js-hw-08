3e04768af9641bd23427821a1b72f049fab620a0
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-02-20 19:42:48 -0800

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const timeFunction = (data) => {
+    let playerSecond = data.seconds
+    localStorage.setItem('videoplayer-current-time', playerSecond);
+};
+
+player.on('timeupdate', throttle(timeFunction, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
