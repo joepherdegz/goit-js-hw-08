5df470f6f9fc284eb37837ed40bc6bce43fde73f
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-02-21 11:13:06 -0800

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
